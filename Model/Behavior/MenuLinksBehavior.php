d97e538689994bfe863fa327414da3b32af4bbc4
Liam Keily <liam@simplywebsites.com>
2014-03-05 23:00:10 +0000

    Delete link when node is deleted
    Fixes #4

--- Model/Behavior/MenuLinksBehavior.php
+++ Model/Behavior/MenuLinksBehavior.php
@@ -1,12 +1,10 @@
 <?php
-
 App::uses('ModelBehavior','Model');
 
 class MenuLinksBehavior extends ModelBehavior {
 
 	public function afterSave(Model $model,$created,$options=array()){
 		$this->Link = ClassRegistry::init('Link');
-
         	if($model->name == 'Node'){
 			if(!empty($model->data['Link'])){
 				$model->data['Link']['link'] =  sprintf(
@@ -25,5 +23,25 @@ class MenuLinksBehavior extends ModelBehavior {
 				}
 			}
 		}
+	}
+
+	public function beforeDelete(Model $model,$cascade=true){
+		$this->Link = ClassRegistry::init('Link');
+		$this->Node = ClassRegistry::init('Node');
+		$node = $this->Node->findById($model->id);
+		if(isset($node['Node'])){
+			$link_url = sprintf(
+			    'plugin:%s/controller:%s/action:%s/type:%s/slug:%s',
+			    'nodes',
+			    'nodes',
+			    'view',
+			    $node['Node']['type'],
+			    $node['Node']['slug']
+			);
+			$this->Link->deleteAll(array(
+				'Link.link'=>$link_url
+			));
+		}
+		Cache::clearGroup('menus','croogo_menus');
 	}
 }
